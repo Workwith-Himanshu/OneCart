98b3b396840068018343127c8d87eca5502f3941
Workwith-Himanshu <sharma.himjaipur@gmail.com>
2025-10-16 22:29:16 +0530

    Ecommerce Website

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  authDomain: "onecart-a7dfb.firebaseapp.com",
+  projectId: "onecart-a7dfb",
+  storageBucket: "onecart-a7dfb.firebasestorage.app",
+  messagingSenderId: "1085082155097",
+  appId: "1:1085082155097:web:34388250955604e6a9d948"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
